60df0c1bf24bc25ff05d479db36d8104ace5ac60
seba <sebastiaanslyper@gmail.com>
2015-01-29 13:28:57 +0100

    aanpassingen Openshift voor GIThub

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -29,19 +29,19 @@ return array(
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
-		),*/
-            
-                'mysql' => array(
-			'host' => getenv('OPENSHIFT_MYSQL_DB_HOST'),
-                        'port' => getenv('OPENSHIFT_MYSQL_DB_PORT'),
-                        'driver'    => 'mysql',
-                        'database'  => getenv('OPENSHIFT_APP_NAME'),
-                        'username'  => getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
-                        'password'  => getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
-                        'charset'   => 'utf8',
-                        'collation' => 'utf8_unicode_ci',
-                        'prefix'    => '',
 		),
+*/
+                'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => getenv('OPENSHIFT_MYSQL_DB_HOST'),
+			'port'      => getenv('OPENSHIFT_MYSQL_DB_PORT'),
+			'database'  => getenv('OPENSHIFT_APP_NAME'),
+			'username'  => getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
+			'password'  => getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+                    ),
 
 		'pgsql' => array(
 			'driver'   => 'pgsql',
